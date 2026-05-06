5cf48abf5183424b05ac12e9145fee1de72f3a3e
Ram-sanehi <vish6933@gmail.com>
2026-05-06 09:34:19 +0530

    update: replace cover page with new image

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -178,7 +178,7 @@ function CoverPage() {
         }}
       >
         <img
-          src="/images/coverpage.png"
+          src="/images/coverpage1.jpeg"
           alt="The Girl Who Forgot Her Earrings"
           style={{
             maxWidth: "100%",
